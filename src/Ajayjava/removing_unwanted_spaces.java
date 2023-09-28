509bc66f787459773bb283f769f6497db0d00588
AJAY SAI RAM <ajaysairamn@gmail.com>
2023-09-28 12:01:33 +0530

    31-8-23

--- src/Ajayjava/removing_unwanted_spaces.java
+++ src/Ajayjava/removing_unwanted_spaces.java
@@ -1,35 +1,29 @@
 package Ajayjava;
-
 import java.util.Scanner;
 
 public class removing_unwanted_spaces {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-        char[] ch = str.toCharArray();
-        int len = str.length();
-        int j;
-        for (int i = 0; i < (len - 1); i++) {
-            if (ch[0] == ' ') {
-                for (j = 0; j < (len - 1); j++)
-                    ch[j] = ch[j + 1];
-                len--;
-                i = -1;
-                continue;
+        StringBuilder result = new StringBuilder();
+        boolean isSpace = true; // Initialize as true to handle leading spaces
 
+        for (char ch : str.toCharArray()) {
+            if (ch != ' ') {
+                result.append(ch);
+                isSpace = false;
+            } else {
+                if (!isSpace) {
+                    result.append(ch);
+                    isSpace = true;
+                }
             }
-            if (ch[i] == ' ' && ch[i + 1] == ' ') {
-                for (j = i; j < (len - 1); j++)
-                    ch[j] = ch[j + 1];
-                ch[j] = '\0';
-                len--;
-                i--;
-            }
+        }
 
+        if (isSpace) {
+            result.deleteCharAt(0); // Remove leading space, if any
         }
-        for (int i = 0; i < len; i++) {
-            System.out.print(String.valueOf(ch[i]));
-        }
+
+        System.out.println(result.toString());
     }
 }
-
--- src/virtusa/WhiteSpace.java
+++ src/virtusa/WhiteSpace.java
@@ -1,6 +1,6 @@
 package virtusa;
 import java.util.*;
-
+//This program is to remove unwanted spaces.
 public class WhiteSpace {
 
 
@@ -9,6 +9,11 @@ public class WhiteSpace {
         String blogName = in.nextLine();
 
         String str = blogName.replaceAll("\\s+", " ");
+//        "\\s+": This is a regular expression pattern. In regular expressions, \\s represents any whitespace character (spaces, tabs,
+//        line breaks, etc.),and + means "one or more times". So, \\s+ matches one or more whitespace characters.
+//
+//        " ": This is the replacement string. It's what the matched patterns (whitespace
+//        characters) will be replaced with. In this case, it's a single space.
 
         System.out.println( str.trim() );
     }
